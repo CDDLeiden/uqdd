82e2234ac27410d86e1438fe5a5b707a901da2ff
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-03-22 21:16:29 +0100

    "test run updates"

--- run.py
+++ run.py
@@ -2,9 +2,21 @@ import pandas as pd
 from papyrus import Papyrus
 from papyrus_scripts.modelling import pcm, qsar
 import xgboost
+from chemutils import generate_ecfp, generate_mol_descriptors
 # generate xc50 and ki dataframes
+# papyrus_xc50 = Papyrus(
+#     path="data/",
+#     chunksize=1000000,
+#     accession=None,
+#     activity_type=["IC50", "EC50"],
+#     protein_class=None,
+#     verbose_files=True
+# )
+# df_xc50 = papyrus_xc50()
+#
+
 papyrus_xc50 = Papyrus(
-    path="data/",
+    path="data/papyrus_filtered_high_quality_xc50_00_preprocessed.csv",
     chunksize=1000000,
     accession=None,
     activity_type=["IC50", "EC50"],
@@ -13,8 +25,19 @@ papyrus_xc50 = Papyrus(
 )
 df_xc50 = papyrus_xc50()
 
+df_xc50 = pd.read_csv("data/papyrus_filtered_high_quality_xc50.csv")
+df_xc50 = generate_ecfp(df_xc50, 2, 1024, False, False)
+df_xc50.to_csv("data/papyrus_filtered_high_quality_xc50_04_with_ECFP.csv", index=False)
 
-# Filtering the top 20 targets in number of datapoints.
+df_xc50 = generate_mol_descriptors(df_xc50, 'smiles', None)
+df_xc50.to_csv("data/papyrus_filtered_high_quality_xc50_05_with_molecular_descriptors.csv", index=False)
+# # calculate ECFP fingerprints
+# self.df_filtered = generate_ecfp(self.df_filtered, 2, 1024, False, False)
+#
+# # calculate mol descriptors
+# self.df_filtered = generate_mol_descriptors(self.df_filtered, 'smiles', None)
+
+# Filtering the top 25 targets in number of datapoints.
 # step 1: group the dataframe by protein target
 grouped = df_xc50.groupby('accession')
 # step 2: count the number of measurements for each protein target
@@ -23,15 +46,17 @@ counts = grouped['accession'].count()
 sorted_counts = counts.sort_values(ascending=False)
 
 # step 4: select the 20 protein targets with the highest counts
-top_targets = sorted_counts.head(20)
+top_targets = sorted_counts.head(25)
 
 # step 5: filter the original dataframe to only include rows corresponding to these 20 protein targets
 filtered_df = df_xc50[df_xc50['accession'].isin(top_targets.index)]
 
-# step 6: iterate over the 20 protein targets
+# step 6: iterate over the 25 protein targets
 for protein_target in top_targets.index:
     # create a new dataframe containing only the rows corresponding to the current protein target
     target_df = df_xc50[df_xc50['protein_target'] == protein_target]
+
+
 
     # model the current protein target using target_df
 
