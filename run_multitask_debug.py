191555771d6c771ea1fe914f17712181ba0bdb70
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-06-02 23:22:36 +0200

    "running scripts"

--- run_multitask_debug.py
+++ run_multitask_debug.py
@@ -1,11 +1,23 @@
+from papyrus import data_preparation
 from models.baselines import run_pipeline
+
+import wandb
     # run_model, hyperparam_search
+
+# preparing the xc50 dataset
+# train_data_xc50, val_data_xc50, test_data_xc50, df_xc50 = data_preparation(papyrus_path='data/', activity='xc50', n_top=20, multitask=True, std_smiles=True, output_path='data/dataset/xc50/', verbose_files=True)
+#
+# # preparing the kx dataset
+# train_data_kx, val_data_kx, test_data_kx, df_kx = data_preparation(papyrus_path='data/', activity='kx', n_top=20, multitask=True, std_smiles=True, output_path='data/dataset/kx/', verbose_files=True)
 
 # running config for multitask debugging
 test_loss = run_pipeline(sweep=False)
 
-# running hyperparam search for multitask debugging
-test_loss = run_pipeline(sweep=True)
+# running config for multitask debugging
+# test_loss = run_pipeline(sweep=False)
+#
+# # running hyperparam search for multitask debugging
+# test_loss = run_pipeline(sweep=True)
 
 # run_model()
 
