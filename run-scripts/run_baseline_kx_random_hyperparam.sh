d5f14a2df36cf710228ec0c93a8a5bfeaa5b9617
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-06-07 13:45:40 +0200

    "bash run scripts fix"

--- run-scripts/run_baseline_kx_random_hyperparam.sh
+++ run-scripts/run_baseline_kx_random_hyperparam.sh
@@ -2,20 +2,12 @@
 
 # Activate the Conda environment
 #conda activate uq-dd
-#pwd
-#cd ..
-#pwd
 
 # Create a logs folder if it doesn't exist
 mkdir -p ../logs
 
 # Get today's date as yyyy-mm-dd format
 today=$(date +%Y-%m-%d)
-
-pwd
-# Change to the directory where the script is located
-cd "$(dirname "$0")"
-pwd
 
 # Run hyperparameter search for kx-random
 python run_baseline.py --activity kx --split random --sweep-count 250 --hyperparam --wandb-project-name "${today}-kx-random-baseline_hyperparam" > ../logs/${today}-kx_random_baseline_hyperparam_output.txt 2>&1
