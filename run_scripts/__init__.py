5e21d7513a088dd0d23d43cafe9ef67f1cc5bb18
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-06-07 17:35:20 +0200

    "inits + imports fix"

--- run_scripts/__init__.py
+++ run_scripts/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from . import run_baseline
-from .. import DATA_DIR, LOGS_DIR, SCRIPTS_DIR, MODELS_DIR
+from .. import DATA_DIR, LOGS_DIR, SCRIPTS_DIR, MODELS_DIR, models
 
 # Append the parent folder path to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
