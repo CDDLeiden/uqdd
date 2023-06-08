23fbe9dbb1c9e104647851bf914fc9198861572c
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-06-08 08:27:41 +0200

    "package bash"

---
+++ uqdd/run_scripts/run_baseline_kx_random_hyperparam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Activate the Conda environment
+#conda activate uq-dd
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Create a logs folder if it doesn't exist
+mkdir -p ../logs
+
+# Get today's date as yyyy-mm-dd format
+today=$(date +%Y-%m-%d)
+
+# Run hyperparameter search for kx-random
+python run_baseline.py --activity kx --split random --sweep-count 250 --hyperparam --wandb-project-name "${today}-kx-random-baseline_hyperparam" > ../logs/${today}-kx_random_baseline_hyperparam_output.txt
---
+++ uqdd/run_scripts/run_baseline_xc50_scaffold_hyperparam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Activate the Conda environment
+#conda activate uq-dd
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Create a logs folder if it doesn't exist
+mkdir -p ../logs
+
+# Get today's date as yyyy-mm-dd format
+today=$(date +%Y-%m-%d)
+
+# Run hyperparameter search for xc50-scaffold
+python -m run_baseline.py --activity xc50 --split scaffold --sweep-count 250 --hyperparam --wandb-project-name "${today}-xc50-scaffold-baseline_hyperparam" > ../logs/${today}-xc50_scaffold_baseline_hyperparam_output.txt
