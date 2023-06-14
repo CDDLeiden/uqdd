81a150cd5495f377d011290684e573f3eb54fb5f
Khalil, Bola [JANBE] <BKhalil@its.jnj.com>
2023-06-15 00:21:44 +0200

    "ensemble runners"

---
+++ uqdd/run_scripts/run_ensemble_xc50_scaffold.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Activate the Conda environment
+#conda activate uq-dd
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Create a logs folder if it doesn't exist
+mkdir -p ../logs
+
+# Get today's date as yyyy-mm-dd format
+today=$(date +%Y-%m-%d)
+
+# Run Ensemble for kx-random
+python run_ensemble.py --activity xc50 --split scaffold --wandb-project-name "${today}-ensemble-models"  --ensemble-size 100 > ../logs/${today}-ensemble_xc50_scaffold_output.txt 2>&1
